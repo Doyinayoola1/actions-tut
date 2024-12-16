a2173c823e3ed701c9cc74ed9631e6ce4a63590a
Doyinayoola1 <128450089+Doyinayoola1@users.noreply.github.com>
2024-12-15 18:14:21 -0600

    Update test_lesserof2.py

--- test_lesserof2.py
+++ test_lesserof2.py
@@ -1,5 +1,8 @@
+#This is a test to check minimum
+
+#This checks lesser file
 def lesser_of_two_evens(a,b):
     if a%2 == 0 and b%2 == 0:
         return min(a,b)
-    else:
-        return max(a,b)
+    
+    return max(a,b)
