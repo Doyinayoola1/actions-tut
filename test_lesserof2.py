84186e8a22f09c67070739cf89c111b818b8bcf4
Doyinayoola1 <128450089+Doyinayoola1@users.noreply.github.com>
2024-12-15 18:17:07 -0600

    Update test_lesserof2.py

--- test_lesserof2.py
+++ test_lesserof2.py
@@ -1,5 +1,8 @@
-#This is a test to check minimum
+"""
+This module contains unit tests for the `lesser_of_two` function.
 
+The `lesser_of_two` function compares two numbers and returns the lesser of the two.
+"""
 #This checks lesser file
 def lesser_of_two_evens(a,b):
     if a%2 == 0 and b%2 == 0:
